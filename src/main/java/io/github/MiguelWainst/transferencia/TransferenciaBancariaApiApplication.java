08101f9cc2fa56a97cf78772ed5202d77e8c3bc4
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-18 19:55:19 -0300

    format: adjust indentation in pom.xml and Java application files

--- src/main/java/io/github/MiguelWainst/transferencia/TransferenciaBancariaApiApplication.java
+++ src/main/java/io/github/MiguelWainst/transferencia/TransferenciaBancariaApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransferenciaBancariaApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransferenciaBancariaApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TransferenciaBancariaApiApplication.class, args);
+    }
 
 }
