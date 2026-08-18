0adeae14f35169e0def8f06c72606605d811d8aa
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-18 19:52:23 -0300

    primeiro commit

---
+++ src/test/java/io/github/MiguelWainst/transferencia/TransferenciaBancariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.MiguelWainst.transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciaBancariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
